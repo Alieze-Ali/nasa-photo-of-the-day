113bd5bf6dc84ed31046cdb471971a18dcd4235b
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-06-22 20:58:00 -0700

    met MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./App.css";
 
+//importing js components to render
+
+import Header from './components/Header'
+import Photo from './components/Photo'
+
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+        app! Have fun <span role="img" aria-label='go!'>🚀</span>
+        </p>
+        {/* importing components here */}
+        <Header />
+        <Photo />
+      
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react' 
+
+
+// setting up Title component
+const Title = (props) => {
+    return (
+        <div>
+            <p>{props.title}</p>
+        </div>
+    )
+
+}
+
+export default Title 
